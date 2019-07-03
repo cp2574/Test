032acb5689b5041715e0bb9845302598b440f2ae
860312862 <860312862@qq.com>
2019-07-03 15:56:32 +0800

    添加了循环

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,6 +11,11 @@ namespace Test
         static void Main(string[] args)
         {
             Console.WriteLine("1");
+
+            for (int i = 0; i < 10; i++)
+			{
+                Console.WriteLine(i);
+			}
         }
     }
 }
